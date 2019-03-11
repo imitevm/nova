b80fa6b53b267d24dce6628edb20b36f2e10c732
Andrew Karpow <andrew.karpow@sap.com>
2019-03-11 11:06:07 +0100

    Revert rais exception for duplicates in cells/build_requests

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -2543,14 +2543,6 @@ class API(base.Base):
             objects=list(filter(filter_method,
                            build_req_instances.objects +
                            insts.objects)))
-
-        if limit is not None and len(instances) != len(build_req_instances) +\
-                len(insts):
-            raise exception.NovaException(
-                _('Duplicate instances found, '
-                  'database inconsistentency between cells and api,'
-                  ' breaks pagination'))
-
         if filter_ip:
             instances = self._ip_filter(instances, filters, orig_limit)
 
